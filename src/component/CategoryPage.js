1f73ed6037aa0d49db2b46ad3b77318e3a46a14d
Sai Han <thesaihan@gmail.com>
2020-06-27 00:49:24 +0630

    Loader & NoData implementation in CategoryPage

--- src/component/CategoryPage.js
+++ src/component/CategoryPage.js
@@ -3,26 +3,33 @@ import "./CategoryPage.css";
 import Axios from "axios";
 import { appendApiKey } from "../utils";
 import Book from "./Book";
+import Loader from "./Loader";
+import NoData from "./NoData";
 
 const CategoryPage = ({match}) => {
 
+  const [isLoading, setIsLoading] = useState(false);
   const [books, setBooks] = useState([]);
   const [catTitle, setCatTitle] = useState("");
 
   const fetchBooks = async (id) => {
+    setIsLoading(true);
     const link = appendApiKey(`/lists/current/${id}.json`);
     try {
       const res = await Axios.get(link);
       if(res.data.results && res.data.results.books && res.data.results.books.length > 0){
         const bookArr = res.data.results.books;
+        setIsLoading(false);
         setCatTitle(res.data.results.display_name);
         setBooks(bookArr);
       } else {
+        setIsLoading(false);
         setCatTitle("");
         setBooks([]);
       }
     } catch (error) {
       console.log(error);
+      setIsLoading(false);
       setCatTitle("");
       setBooks([]);
     }
@@ -41,7 +48,8 @@ const CategoryPage = ({match}) => {
           </div>
         </div>
         {
-          catTitle && (
+          isLoading ? <Loader /> :
+          books && books.length>0 ? (
             <div className="row justify-content-center">
               <div className="col-md-10">
                 <h3 className="text-left">{catTitle}</h3>
@@ -51,7 +59,7 @@ const CategoryPage = ({match}) => {
                 }
               </div>
             </div>
-          )
+          ) : <NoData />
         }
         
       </div>
--- src/component/Loader.js
+++ src/component/Loader.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./Loader.css";
 
 const Loader = () => {
-  return <div class="Loader mt-5 mx-auto"></div>;
+  return <div className="Loader mt-5 mx-auto"></div>;
 };
 
 export default Loader;
