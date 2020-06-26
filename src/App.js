64eef3ea920a191ef19c1f96ba16c3f37ef3f688
Sai Han <thesaihan@gmail.com>
2020-06-26 22:55:30 +0630

    Integrate components into App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import './App.css';
 import HomePage from './component/HomePage';
+import CategoryPage from './component/CategoryPage';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <HomePage />
+    <div className="App text-secondary">
+      <Router>
+        <Switch>
+          <Route exact path="/" component={HomePage}/>
+          <Route exact path="/browse" component={HomePage}/>
+          <Route exact path="/category" component={CategoryPage}/>
+          <Route exact path="/category/:id" component={CategoryPage}/>
+        </Switch>
+      </Router>
     </div>
   );
 }
--- src/component/HomePage.js
+++ src/component/HomePage.js
@@ -1,10 +1,54 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import "./HomePage.css";
+import Axios from "axios";
+import { appendApiKey, colors } from "../utils";
+import Genre from "./Genre";
 
 const HomePage = () => {
+
+  const [genres, setGenres] = useState([]);
+
+  const fetchGenres = async () => {
+    const link = appendApiKey("/lists/names.json");
+    try {
+      [].slice()
+      const res = await Axios.get(link);
+      const genArr = res.data.results;
+      if(genArr && genArr.length > 0){
+        setGenres(genArr.slice(genArr.length-1-20, genArr.length-1));
+      }
+    } catch (error) {
+      console.log(error);
+      setGenres([]);
+    }
+  };
+
+  useEffect(() => {
+    fetchGenres();
+  }, []);
+
   return (
     <div className="HomePage">
-      <h2>Home Page</h2>
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-md-12">
+            <h2 className="mt-3 mb-5">New York Times Best Sellers</h2>
+          </div>
+        </div>
+        <div className="row justify-content-center">
+          <div className="col-md-10">
+            <h3 className="text-left">Browse Category</h3>
+            <hr />
+            {
+              genres.map(genre => <Genre 
+                key={genre.list_name_encoded} 
+                genre={genre}
+                bgColor={colors[Math.floor(Math.random() * colors.length)]}
+                />)
+            }
+          </div>
+        </div>
+      </div>
     </div>
   )
 };
