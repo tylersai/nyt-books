278eb0d9931c0153b5cd1ef6e1135dfc4722a815
Sai Han <thesaihan@gmail.com>
2020-06-27 00:12:01 +0630

    Book card fixes

--- src/component/Book.js
+++ src/component/Book.js
@@ -5,19 +5,19 @@ const Book = ({book}) => {
 
   return (
   <div className="Book d-inline-block card m-3 shadow" 
-    style={{maxWidth : "450px", overflowY: "hidden", maxHeight: book.book_image_height+"px"}}>
+    style={{maxWidth : "450px"}}>
     <div className="row no-gutters">
-      <div className="col-md-4">
+      <div className="col-4">
         <img src={book.book_image} className="card-img" alt="BookImg"/>
       </div>
-      <div className="col-md-8">
+      <div className="col-8">
         <div className="card-body text-left">
-          <h5 className="card-title text-info">{book.title}</h5>
+          <h5 className="card-title text-dark">{book.title}</h5>
           <p className="card-text">
             {book.description}
           </p>
           <p className="card-text">
-            <small className="text-muted">
+            <small className="text-info">
               {book.author}
             </small>
           </p>
