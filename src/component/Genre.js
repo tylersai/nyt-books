eab0b7885a91b2a476d32063460fb0ccc8440849
Sai Han <thesaihan@gmail.com>
2020-06-26 23:04:42 +0630

    Genre block style

--- src/component/Genre.js
+++ src/component/Genre.js
@@ -1,12 +1,15 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./Genre.css";
 
 const Genre = ({genre, bgColor}) => {
 
   return (
-    <div style={{backgroundColor : bgColor}} className="Genre d-inline-flex m-3 rounded justify-content-center align-items-center p-3">
-      <h5 className="text-dark">{genre.display_name}</h5>
-    </div>
+    <Link to={`/category/${genre.list_name_encoded}`}>
+      <div style={{backgroundColor : bgColor}} className="Genre d-inline-flex m-3 shadow justify-content-center align-items-center p-3">
+        <h5 className="text-dark">{genre.display_name}</h5>
+      </div>
+    </Link>
   )
 };
 
