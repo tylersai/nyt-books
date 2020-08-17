41fa365121deb56e8423f8f7969f22257c243780
Sai Han <45680003+thesaihan@users.noreply.github.com>
2020-08-17 23:12:46 +0630

    Remove desc text for test.

--- src/component/Title.js
+++ src/component/Title.js
@@ -5,7 +5,6 @@ const Title = () => {
     <div className="Title row">
       <div className="col-md-12">
         <h2 className="mt-3 mb-2 text-dark">New York Times Best Sellers</h2>
-        <h4 className="mb-5 text-dark">Description goes here (by SE Team)</h4>
       </div>
     </div>
   );
